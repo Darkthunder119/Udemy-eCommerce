5c7ad5b132695c7630a93d8d336ad60cf1eb9039
Darkthunder119 <gurtajchhabra@gmail.com>
2020-08-26 23:57:56 -0400

    Added base components upto section 4

---
+++ ecommerce-app/src/components/Directory/Directory.jsx
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import './Directory.scss';
+import MenuItem from '../MenuItem/MenuItem';
+
+const Directory = () =>{
+    const [data, setData] = useState([
+        {
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+          linkUrl: 'shop/hats'
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+          linkUrl: 'shop/jackets'
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3,
+          linkUrl: 'shop/sneakers'
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          id: 4,
+          linkUrl: 'shop/womens'
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          id: 5,
+          linkUrl: 'shop/mens'
+        }
+      ]
+      );
+
+      return(
+          <div className="directory-menu">
+              {
+                  data && data.map(val=><MenuItem title={val.title} imageUrl={val.imageUrl} size={val.size} id={val.id} linkUrl={val.linkUrl} key={val.id}/>)
+              }
+          </div>
+      );
+}
+
+export default Directory;
